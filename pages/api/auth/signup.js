395b48b7dfd32cf1463febde6b3db8d9f952a0e5
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-04-18 18:22:16 -0700

    new user successfully posted to mongodb db

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,23 +1,27 @@
 import { connect } from "http2";
 import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db,js";
 
 async function handler(req, res){
+    if(req.method !== 'POST'){
+        return;
+    }
     const data = req.body;
     const {email, password} = data; 
     if(!email || !email.includes('@') || !password || password.trim().length < 5){
         res.status(422).json({message: 'Invalid input - password'})
         return;
     }
-    const client = await connectToDtatbase(); 
+    const client = await connectToDatabase(); 
 
     const db = client.db();
 
     // authentication 
-    const hashedPassword = hashPassword(password); 
+    const hashedPassword = await hashPassword(password); 
 
     const result = await db.collection('users').insertOne({
         email: email,
-        password: password
+        password: hashPassword
     }); 
 
     res.status(201).json({message: 'Created user!'}); 
