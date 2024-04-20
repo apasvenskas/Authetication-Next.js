0a1bd97e8d831d53d57500678a51cc1fde8b423a
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-04-19 22:46:12 -0700

    session auth protection fully implemented

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,29 +1,31 @@
-import { useSession, getSession } from 'next-auth/react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+// import { getSession } from 'next-auth/react';
 
 function UserProfile() {
-  // Redirect away if NOT auth
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadedSession, setLoadedSession] = useState();
+  // // Redirect away if NOT auth
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getSession().then(session => {
-      // setLoadedSession(session)
-      if(!session){
-        window.location.href = '/auth'; 
-      } else {
-        setIsLoading(false)
-      }
-    })
-  }, [])
+  // useEffect(() => {
+  //   getSession().then(session => {
+  //     // setLoadedSession(session)
+  //     if(!session){
+  //       window.location.href = '/auth'; 
+  //     } else {
+  //       setIsLoading(false)
+  //     }
+  //   })
+  // }, [])
 
-  // const { data: session, loading} = useSession();
+  // // const { data: session, loading} = useSession();
 
-  if(isLoading){
-    return <p className={classes.profile}>Loading...</p>
-  }
+  // if(isLoading){
+  //   return <p className={classes.profile}>Loading...</p>
+  // }
+
+  // used a getServerSideProps function in the pages/profile.js file instead
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
